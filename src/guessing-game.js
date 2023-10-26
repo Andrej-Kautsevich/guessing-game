b96be65c5c31618bcbc1ecb3b755af201b928496
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-10-26 17:38:45 +0300

    feat: compleate task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0;
+        this.right = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        return this.mid = Math.round((this.left + this.right) / 2);
     }
 
     lower() {
-
+        this.right = this.mid;
     }
 
     greater() {
-
+        this.left = this.mid;
     }
 }
 
